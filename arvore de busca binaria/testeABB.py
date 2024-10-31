285567db7eebf8c092b7e2d793c0b32e35b26631
Tiago Boldori <tiagoboldori@gmail.com>
2024-10-31 10:18:15 -0300

    update

--- arvore de busca binaria/testeABB.py
+++ arvore de busca binaria/testeABB.py
@@ -1,16 +1,12 @@
+
 from elemento import Elemento
 from nodeb import NodeABB
 from arvorebb import ArvoreBB
 
-
-
 arvore = ArvoreBB()
 
-
-
-
 while True:
-    print('Opções: \n 1-Criar/Adicionar nós a arvore. \n 2-Remover nó da arvore. \n 3- Procurar elemento na arvore. \n 4-Mostrar Arvore. \n 5-Mostrar maior e menor elemento da arvore. \n (-1 para sair)')
+    print('Opções: \n 1-Criar/Adicionar nós a arvore. \n 2-Remover nó da arvore. \n 3- Procurar elemento na arvore. \n 4-Mostrar Arvore. \n 5-Mostrar maior e menor elemento da arvore. \n 6-Mostrar numero de elementos na arvore \n 7-Mostrar soma das chaves da arvore \n (-1 para sair)')
     info = int(input('Digite um numero correspondente as opções: '))
     if info ==-1:
         break
@@ -30,6 +26,7 @@ while True:
 
             e = None
             n = None
+            
     if info == 2:
         
         i = int(input('Chave do nó a ser removido: '))
@@ -51,9 +48,8 @@ while True:
         arvore.mostraMaior(arvore.getRaiz())
         arvore.mostraMenor(arvore.getRaiz())
     
-
-
-
-
-
-
+    if info ==6:
+        print('quantidade de nos: ', arvore.contaNos(arvore.getRaiz()))
+    
+    if info == 7:
+        print('soma das chaves: ', arvore.contaChave(arvore.getRaiz()))
